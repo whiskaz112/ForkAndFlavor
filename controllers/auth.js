f9be5ffadbca2408856dde4c48a2797c68091d84
YhongYhong <prommmm44@gmail.com>
2024-11-19 16:33:22 +0700

    add logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ exports.register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
     await authService.registerUser(username, email, password);
-    res.status(201).json({ message: 'User registered successfully', success: true});
+    res.status(201).json({ message: 'User registered successfully', success: true });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -22,59 +22,70 @@ exports.login = async (req, res) => {
       sameSite: 'strict',
     });
 
-    res.json({ message: 'Login successful', success: true});
+    res.json({ message: 'Login successful', success: true });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
 
+exports.logout = async (req, res) => {
+  try {
+    res.cookie('token', '', { maxAge: 1 });
+    res.json({ message: 'Logout successful', success: true });
+  }
+  catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// not used
 exports.getUser = async (req, res) => {
-    try {
-        console.log('start using token');
-        const authToken = req.cookies.token;
-        console.log('token', authToken)
-        const verifyUser = jwt.verify(authToken, secret);
-        const checkUser = await User.findOne({ email: verifyUser.user.email });
-        if (!checkUser) {
-            throw { message: 'user not found' };
-        }
-        console.log('this checkUser: ', checkUser)
-        res.status(200).json(checkUser);
+  try {
+    console.log('start using token');
+    const authToken = req.cookies.token;
+    console.log('token', authToken)
+    const verifyUser = jwt.verify(authToken, secret);
+    const checkUser = await User.findOne({ email: verifyUser.user.email });
+    if (!checkUser) {
+      throw { message: 'user not found' };
     }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Server Error',
-            err
-        });
-    }
+    console.log('this checkUser: ', checkUser)
+    res.status(200).json(checkUser);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Server Error',
+      err
+    });
+  }
 };
 
 exports.uploadPic = async (req, res) => {
-    res.json(req.file)
-    // try {
-    //     const formData = req.body;
-    //     const userId = formData.userId;
-    //     const image = req.file;
+  res.json(req.file)
+  // try {
+  //     const formData = req.body;
+  //     const userId = formData.userId;
+  //     const image = req.file;
 
-    //     console.log('formData: ', formData)
-    //     console.log('userId: ', userId)
-    //     console.log('image: ', image)
-        
-    //     const user = await User.findById(userId);
-    //     if (!user) {
-    //         return res.status(404).json({ message: 'User not found' });
-    //     }
+  //     console.log('formData: ', formData)
+  //     console.log('userId: ', userId)
+  //     console.log('image: ', image)
 
-    //     user.profilePic = image.buffer; // Assuming you want to save the image buffer
-    //     await user.save();
-    //     res.status(201).json({ message: 'New image uploaded!!'});
-    // }
-    // catch (err) {
-    //     console.log(err);
-    //     res.status(500).json({
-    //         message: 'Server Error',
-    //         err
-    //     })
-    // }
+  //     const user = await User.findById(userId);
+  //     if (!user) {
+  //         return res.status(404).json({ message: 'User not found' });
+  //     }
+
+  //     user.profilePic = image.buffer; // Assuming you want to save the image buffer
+  //     await user.save();
+  //     res.status(201).json({ message: 'New image uploaded!!'});
+  // }
+  // catch (err) {
+  //     console.log(err);
+  //     res.status(500).json({
+  //         message: 'Server Error',
+  //         err
+  //     })
+  // }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -4,22 +4,23 @@ const router = express.Router();
 // handle file upload
 const multer = require('multer');
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads');
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
 });
 const upload = multer({ storage });
 
 const { authenticate, authorize } = require('../middleware/auth');
-const { register, login, getUser, uploadPic } = require('../controllers/auth');
+const { register, login, logout, getUser, uploadPic } = require('../controllers/auth');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/getUser', getUser);
 router.post('/uploadPic', upload.single('myFile'), uploadPic);
+router.get('/logout', logout);
 
 router.get('/user', authenticate, authorize(['user', 'admin']), (req, res) => {
   res.json({ message: 'Welcome User' });
